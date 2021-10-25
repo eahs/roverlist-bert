17c8e8720251a8a4498abab2609ee2ec15cc7e0e
Justin Costenbader <justincostenbader@gmail.com>
2021-10-25 11:33:37 -0400

    Revert and new user input for words

--- RoverList/Program.cs
+++ RoverList/Program.cs
@@ -13,13 +13,45 @@ namespace RoverList
             RoverList<string> list = new RoverList<string>();
 
             // TODO:  Implement the RoverList class
+            
             // TODO:  Create a RoverList and then fill it with 16 words
-
+            list.Add("bert");
+            list.Add("skirt");
+            list.Add("dirt");
+            list.Add("flirt");
+            list.Add("shirt");
+            list.Add("exert");
+            list.Add("concert");
+            list.Add("hurt");
+            list.Add("alert");
+            list.Add("kurt");
+            list.Add("introvert");
+            list.Add("dessert");
+            list.Add("yogurt");
+            list.Add("divert");
+            list.Add("burnt");
+            list.Add("assert");
             // TODO:  Print out the list
+            list.ListNodes();
 
             // TODO:  Prompt the user to input words, add those words to the list until they enter the word "done"
+            Console.WriteLine("Enter a word to be added to the list. Enter 'done' when complete.");
+            
             // TODO:  Print out the list
-
+            String input = "";
+            while(input != "done")
+            {
+                if(input == "done")
+                {
+                    break;
+                }
+                else 
+                { 
+                    list.Add(input);
+                    input = Console.ReadLine();
+                }
+            }
+            
             // TODO:  Prompt the user to input words, add those words to the FRONT of the list until they enter the word "done"
             // TODO:  Print out the list
 
